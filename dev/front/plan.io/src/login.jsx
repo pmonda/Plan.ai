8744c974139535e3dce31f56a1185f62795e53d2
kpedd <kpeddakotla@gmail.com>
2024-09-12 14:36:57 -0400

    Login Front end Finished

--- dev/front/plan.io/src/login.jsx
+++ dev/front/plan.io/src/login.jsx
@@ -1,27 +1,58 @@
-import { useState } from 'react';
-import './Login.css'
+import React, { useState } from 'react';
+import './Login.css';
+import bannerlogo from '../src/assets/Plan.IO__1_-removebg-preview.png';
 
 const logins = {
   'admin': 'test',
-  'pmonda': 'password'
+  'pmonda': 'password',
+  'kpeddako': '12345',
+  'peda': '@!@!'
 };
 
 function checkPassword(username, password) {
-  if(logins[username] === password) {
+  if (logins[username] === password) {
     console.log(username + ' logged in');
+  } else {
+    console.log('Incorrect login');
   }
-    
 }
+
 export default function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    checkPassword(username, password);
+  };
+  
+  const handleRegister = () => {
+    
+  };
+
   return (
-  <div>
-    <h1>Login</h1>
-    <input id="user" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="username"></input>
-    <br/>
-    <input id="pwd" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="password"></input>
-    <br/>
-    <button onClick={checkPassword(username, password)}>Login</button>
-  </div>);
+    <div className="login-container">
+      <img className="logo" src={bannerlogo} alt="Logo" />
+      <h1>Login</h1>
+      <p>Welcome to Plan.io! <br></br> Please log in to continue.</p>
+      <input 
+        id="user" 
+        value={username} 
+        onChange={(e) => setUsername(e.target.value)} 
+        placeholder="Username" 
+      />
+      <input 
+        id="pwd" 
+        type="password" 
+        value={password} 
+        onChange={(e) => setPassword(e.target.value)} 
+        placeholder="Password" 
+      />
+      <br/>
+      <br/>
+      <button onClick={handleLogin}>Login</button>
+      <br/>
+      <br/>
+      <button onClick={handleRegister}>Register</button>
+    </div>
+  );
 }
