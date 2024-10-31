52ee763e74fb304ccfd4314aaaa7da35a8e3f8ad
pmondaPUWL <pmonda@purdue.edu>
2024-10-31 15:06:57 -0400

    added some dependncies in package-lock for CI fixes

--- dev/front/plan.io/src/service/AuthService.js
+++ dev/front/plan.io/src/service/AuthService.js
@@ -1,23 +1,23 @@
     const user = null;
     const token = null;
 
-export function getUser() {
-    const user = sessionStorage.getItem('user');
-    if (user === 'undefined' || !user) {
-        return null;
+    export function getUser() {
+        const user = sessionStorage.getItem('user');
+        if (user === 'undefined' || !user) {
+            return null;
+        }
+        else {
+            return JSON.parse(user);
+        }
     }
-    else {
-        return JSON.parse(user);
+    export function getToken() {
+        return sessionStorage.getItem('token');
     }
-}
-export function getToken() {
-    return sessionStorage.getItem('token');
-}
-export function setUserSession() {
-    sessionStorage.setItem('user', JSON.stringify(user));
-    sessionStorage.setItem('token', token);
-}
-export function resetUserSession() {
-    sessionStorage.removeItem('user');
-    sessionStorage.removeItem('token');
-}
+    export function setUserSession() {
+        sessionStorage.setItem('user', JSON.stringify(user));
+        sessionStorage.setItem('token', token);
+    }
+    export function resetUserSession() {
+        sessionStorage.removeItem('user');
+        sessionStorage.removeItem('token');
+    }
